fc183309978030e56ee67e8dfeacaaf1f7924c6a
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2023-05-27 13:48:32 -0300

    Blog 425px Responsive

--- src/Blog e Posts/Blog/script.js
+++ src/Blog e Posts/Blog/script.js
@@ -22,3 +22,31 @@ function voltar(){
 }
 
 
+let menu_hamb = document.getElementById('menu_hamb')
+let btn_hamb = document.getElementById('btn_hamb')
+let btn_close = document.getElementById('btn_close')
+
+btn_hamb.addEventListener('click', openMenuHamb)
+
+function openMenuHamb(){
+   menu_hamb.style.display = 'flex'
+}
+
+btn_close.addEventListener('click', closeMenuHamb)
+
+function closeMenuHamb(){
+   menu_hamb.style.display = 'none'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
