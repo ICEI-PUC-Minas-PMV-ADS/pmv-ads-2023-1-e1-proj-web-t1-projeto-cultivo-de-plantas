fd2bec6aee9f97dcb764f4db744dfab6add10d73
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2023-05-27 22:05:50 -0300

    Post cacto responsive.

--- src/Blog e Posts/Posts/Cacto/script.js
+++ src/Blog e Posts/Posts/Cacto/script.js
@@ -74,4 +74,26 @@ function pragas() {
      image_three.style.display = 'flex'
   
   }
+
+  
+let menu_hamb = document.getElementById('menu_hamb')
+let btn_hamb = document.getElementById('btn_hamb')
+let btn_close = document.getElementById('btn_close')
+
+btn_hamb.addEventListener('click', openMenuHamb)
+
+function openMenuHamb(){
+   menu_hamb.style.display = 'flex'
+}
+
+btn_close.addEventListener('click', closeMenuHamb)
+
+function closeMenuHamb(){
+   menu_hamb.style.display = 'none'
+}
+
+
+
+
+
   
