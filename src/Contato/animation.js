93d853b58a86fdb73fff5cc680b4d2cddb82a5f1
Bruna Santos <s_bruna@live.com>
2023-05-29 16:03:56 -0300

    modal com mensagem de enviado

--- src/Contato/animation.js
+++ src/Contato/animation.js
@@ -1,3 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const openModalButton = document.getElementById('openModal');
+  const modal = document.getElementById('myModal');
+  const closeButton = document.getElementsByClassName('close')[0];
+
+  // Abrir modal ao clicar no botão
+  openModalButton.addEventListener('click', function() {
+    modal.style.display = 'block';
+  });
+
+  // Fechar modal ao clicar no botão de fechar
+  closeButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+  });
+
+  // Fechar modal ao clicar fora dele
+  window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
+
 /*ANIMAÇÃO*/
 
 const inputs = document.querySelectorAll(".input");
@@ -18,4 +42,3 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
-
