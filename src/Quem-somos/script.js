6adcd412401b3068916598f67566987091f9b20c
Bruna Santos <s_bruna@live.com>
2023-05-30 17:13:14 -0300

    add menu hamburguer e pasta 'script.js'

---
+++ src/Quem-somos/script.js
@@ -0,0 +1,10 @@
+/* MENU MOBILE */
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+    }else{
+        menuMobile.classList.add('open')
+    }
+  }
+  
