cb8d230b036888da3b98aabc00895dec60481d72
Tayane Milagres <devtaay@gmail.com>
2023-05-30 20:55:27 -0300

    Linkagem do Header

--- src/Cadastro page/cadastro.js
+++ src/Cadastro page/cadastro.js
@@ -150,6 +150,13 @@ function sucessoValidation(input, message) {
    small.innerText = message
 }
 
-
-
+//Menu Hamburguer
+function menuShow() {
+   let menuMobile = document.querySelector('.mobile-menu');
+   if (menuMobile.classList.contains('open')) {
+       menuMobile.classList.remove('open')
+   }else{
+       menuMobile.classList.add('open')
+   }
+}
 
