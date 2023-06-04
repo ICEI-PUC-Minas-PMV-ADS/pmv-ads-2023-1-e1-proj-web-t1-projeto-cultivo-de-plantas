e8c3e685a2e29d0673346e90cc1fd88a64b3ca51
André Guilherme <andre.guilherme2009@gmail.com>
2023-06-04 18:57:16 -0300

    Atualização

--- src/Login page/login.js
+++ src/Login page/login.js
@@ -10,10 +10,9 @@ function checkValida() {
 
     const emailLog = document.getElementById("emailValida").value;
     const senhaLog = document.getElementById("senhaValida").value;
-
-
-    var listaCad = []
-    var userValid = {
+    let msgError = document.getElementById('msgError')
+    
+var userValid = {
         email: emailLog ,
         senha:  senhaLog
     }
@@ -22,7 +21,7 @@ function checkValida() {
 let listaCad = JSON.parse(localStorage.getItem("ListaCad"))
     
 
-const usuarioEncontrado = listaCad.find((element) => user.emailCad === userValid.email && user.senhaCad === userValid.senha);
+const usuarioEncontrado = listaCad.find((element) => element.emailCad === userValid.email && element.senhaCad === userValid.senha);
 
 if (usuarioEncontrado) {
    
@@ -32,7 +31,9 @@ if (usuarioEncontrado) {
 else {
     
     console.log("Login falhou");
-    msgError.textContent = "Email ou senha inválidos";
+    msgError.setAttribute('style', 'display: block');
+    msgError.innerHTML = '<strong>Email ou senha inválidos</strong>';
+    
 }
 
 }
