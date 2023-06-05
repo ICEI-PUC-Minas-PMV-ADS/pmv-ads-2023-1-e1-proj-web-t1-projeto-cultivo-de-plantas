806f31e24aec927102f8919687bf030965d84e43
Bruna Santos <s_bruna@live.com>
2023-06-05 00:49:12 -0300

    fixar o modal

--- src/Contato/script.js
+++ src/Contato/script.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const modal = document.getElementById('myModal');
   const closeButton = document.getElementsByClassName('close')[0];
 
-  /
   openModalButton.addEventListener('click', function(event) {
     event.preventDefault(); 
 
