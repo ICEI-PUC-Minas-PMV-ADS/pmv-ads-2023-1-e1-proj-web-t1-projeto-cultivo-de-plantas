043183e878c9b9a6a852b2576de3bc43d8b94420
Bruna Santos <s_bruna@live.com>
2023-06-05 00:44:20 -0300

    fixar modal

--- src/Contato/script.js
+++ src/Contato/script.js
@@ -8,30 +8,39 @@ function menuShow() {
   }
 }
 
-
 /* MODAL */
+
 document.addEventListener('DOMContentLoaded', function() {
   const openModalButton = document.getElementById('openModal');
   const modal = document.getElementById('myModal');
   const closeButton = document.getElementsByClassName('close')[0];
 
-  // Abrir modal ao clicar no botão
-  openModalButton.addEventListener('click', function() {
+  /
+  openModalButton.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
     modal.style.display = 'block';
   });
 
-  // Fechar modal ao clicar no botão de fechar
+  
   closeButton.addEventListener('click', function() {
     modal.style.display = 'none';
   });
 
-  // Fechar modal ao clicar fora dele
+ 
+  modal.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+
+  
   window.addEventListener('click', function(event) {
     if (event.target == modal) {
       modal.style.display = 'none';
     }
   });
 });
+
+
 
 /*ANIMAÇÃO*/
 
