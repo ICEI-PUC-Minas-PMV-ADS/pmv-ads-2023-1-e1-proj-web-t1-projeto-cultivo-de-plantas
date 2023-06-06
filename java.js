35ea6a52498a28310fbea212e6c91c9b938d6174
João <joao.almeida.1439563@sga.pucminas.br>
2023-06-05 22:34:52 -0300

    branch home
    menu hamburger blue

---
+++ java.js
@@ -0,0 +1,8 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+    }else{
+        menuMobile.classList.add('open')
+    }
+}
