30d4b2820278bdf30db64041abe9a26f51084ec2
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2023-06-08 16:31:49 -0300

    Commit seachBar 2

--- src/Blog e Posts/Blog/script.js
+++ src/Blog e Posts/Blog/script.js
@@ -1,5 +1,116 @@
 
 
+ 
+
+
+
+
+
+
+var itemURLs = {
+   Bromélia: '../Posts/Bromélia/Index.html',
+   Girassol: '../Posts/Girassol/Index.html',
+   Cacto: '../Posts/Cacto/Index.html',
+   Cebola: '../Posts/Cebola/Index.html',
+   Hera_Inglesa: '../Posts/Hera-Inglesa/Index.html',
+   Hortência: '../Posts/Hortência/Index.html',
+   Iresine: '../Posts/Iresine/Index.html',
+   Jasmim: '../Posts/Jasmim/Index.html',
+   Lavanda: '../Posts/Lavanda/Index.html',
+   Lírio: '../Posts/Lírio/Index.html',
+   Neoregélia: '../Posts/Neoregélia/Index.html',
+   Orquidea: '../Posts/Orquidea/Index.html',
+   Palmeira_Dama: '../Posts/Palmeira-Dama/Index.html',
+   Pitanga: '../Posts/Pitanga/Index.html',
+   Roseira: '../Posts/Roseira/Index.html',
+   Samambaia: '../Posts/Samambaia/Index.html',
+   Tomate: '../Posts/Tomate/Index.html',
+   Torênia: '../Posts/Torênia/Index.html',
+   Trapoeraba: '../Posts/Trapoeraba/Index.html'
+};
+
+var plantas = ['Bromélia', 'Cacto', 'Girassol', 'Cebola', 'Espada-de-são-Jorge', 'Hera_Inglesa', 'Hortência', 'Iresine', 'Jasmim', 'Lavanda' , 'Lírio', 'Neoregélia', 'Orquidea','Palmeira_Dama','Pitanga' ,'Roseira' ,'Samambaia','Tomate','Torênia','Trapoeraba'];
+localStorage.setItem('meuVetor', JSON.stringify( plantas));
+
+document.getElementById('searchInput').addEventListener('input', function() {
+   var searchTerm = this.value.toLowerCase();
+   var storedVetor = JSON.parse(localStorage.getItem('meuVetor'));
+   var searchResults = [];
+ 
+   // Percorre o vetor e verifica se o termo de pesquisa está contido em cada item
+   for (var i = 0; i < storedVetor.length; i++) {
+     var item = storedVetor[i].toLowerCase();
+     if (item.indexOf(searchTerm) !== -1) {
+       searchResults.push(storedVetor[i]);
+     }
+   }
+ 
+   // Atualiza a lista de resultados na página
+   var resultsElement = document.getElementById('searchResults');
+   resultsElement.innerHTML = '';
+ 
+   for (var j = 0; j < searchResults.length; j++) {
+     var li = document.createElement('li');
+     
+     var link = document.createElement('a');
+     var itemText = searchResults[j];
+ 
+     if (itemURLs.hasOwnProperty(itemText)) {
+       link.href = itemURLs[itemText];
+     } else {
+       link.href = '#'; // Defina um URL padrão para itens não mapeados
+     }
+     li.classList.add('searchResult');
+ 
+     link.textContent = itemText;
+ 
+     link.addEventListener('click', function() {
+       window.location.href = this.href;
+     });
+ 
+     li.appendChild(link);
+     resultsElement.appendChild(li);
+   }
+
+
+
+
+ });
+
+ 
+
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var carousel = document.getElementById('carousel_first')
 var carousel_dois = document.getElementById('carousel_second')
 var btn_right = document.getElementById('rigth_btn')
