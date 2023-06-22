982fda0f87a46a866b11ae43cf26589f0a00e299
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2023-06-22 20:10:17 -0300

    cadastro barra de pesquisa

--- src/Cadastro page/cadastro.js
+++ src/Cadastro page/cadastro.js
@@ -1,30 +1,30 @@
 var itemURLs = {
    Bromélia: '../Blog e Posts/Posts/Bromélia/Index.html',
-  Espada_de_são_Jorge: '../Blog e Posts/Posts/Espada-de-são-jorge/index.html',
-  Girassol: '../Blog e Posts/Posts/Girassol/Index.html',
-  Cacto: '../Blog e Posts/Posts/Cacto/Index.html',
-  Cebola: '../Blog e Posts/Posts/Cebola/Index.html',
-  Hera_Inglesa: '../Blog e Posts/Posts/Hera-Inglesa/Index.html',
-  Hortência: '../Blog e Posts/Posts/Hortência/Index.html',
-  Iresine: '../Blog e Posts/Posts/Iresine/Index.html',
-  Jasmim: '../Blog e Posts/Posts/Jasmim/Index.html',
-  Lavanda: '../Blog e Posts/Posts/Lavanda/Index.html',
-  Lírio: '../Blog e Posts/Posts/Lírio/Index.html',
-  Neoregélia: '../Blog e Posts/Posts/Neoregélia/Index.html',
-  Orquidea: '../Blog e Posts/Posts/Orquidea/Index.html',
-  Palmeira_Dama: '../Blog e Posts/Posts/Palmeira-Dama/Index.html',
-  Pitanga: '../Blog e Posts/Posts/Pitanga/Index.html',
-  Roseira: '../Blog e Posts/Posts/Roseira/Index.html',
-  Samambaia: '../Blog e Posts/Posts/Samambaia/Index.html',
-  Tomate: '../Blog e Posts/Posts/Tomate/Index.html',
-  Torênia: '../Blog e Posts/Posts/Torênia/Index.html',
-  Trapoeraba: '../Blog e Posts/Posts/Trapoeraba/Index.html'
-};
-
-var plantas = ['Bromélia', 'Cacto', 'Girassol', 'Cebola', 'Espada-de-são-Jorge', 'Hera_Inglesa', 'Hortência', 'Iresine', 'Jasmim', 'Lavanda' , 'Lírio', 'Neoregélia', 'Orquidea','Palmeira_Dama','Pitanga' ,'Roseira' ,'Samambaia','Tomate','Torênia','Trapoeraba'];
-localStorage.setItem('meuVetor', JSON.stringify( plantas));
-
-document.getElementById('searchInput').addEventListener('input', function() {
+   Espada_de_são_Jorge: '../Blog e Posts/Posts/Espada-de-são-jorge/index.html',
+   Girassol: '../Blog e Posts/Posts/Girassol/Index.html',
+   Cacto: '../Blog e Posts/Posts/Cacto/Index.html',
+   Cebola: '../Blog e Posts/Posts/Cebola/Index.html',
+   Hera_Inglesa: '../Blog e Posts/Posts/Hera-Inglesa/Index.html',
+   Hortência: '../Blog e Posts/Posts/Hortência/Index.html',
+   Iresine: '../Blog e Posts/Posts/Iresine/Index.html',
+   Jasmim: '../Blog e Posts/Posts/Jasmim/Index.html',
+   Lavanda: '../Blog e Posts/Posts/Lavanda/Index.html',
+   Lírio: '../Blog e Posts/Posts/Lírio/Index.html',
+   Neoregélia: '../Blog e Posts/Posts/Neoregélia/Index.html',
+   Orquidea: '../Blog e Posts/Posts/Orquidea/Index.html',
+   Palmeira_Dama: '../Blog e Posts/Posts/Palmeira-Dama/Index.html',
+   Pitanga: '../Blog e Posts/Posts/Pitanga/Index.html',
+   Roseira: '../Blog e Posts/Posts/Roseira/Index.html',
+   Samambaia: '../Blog e Posts/Posts/Samambaia/Index.html',
+   Tomate: '../Blog e Posts/Posts/Tomate/Index.html',
+   Torênia: '../Blog e Posts/Posts/Torênia/Index.html',
+   Trapoeraba: '../Blog e Posts/Posts/Trapoeraba/Index.html'
+ };
+ 
+ var plantas = ['Bromélia', 'Cacto', 'Girassol', 'Cebola', 'Espada-de-são-Jorge', 'Hera_Inglesa', 'Hortência', 'Iresine', 'Jasmim', 'Lavanda' , 'Lírio', 'Neoregélia', 'Orquidea','Palmeira_Dama','Pitanga' ,'Roseira' ,'Samambaia','Tomate','Torênia','Trapoeraba'];
+ localStorage.setItem('meuVetor', JSON.stringify(plantas));
+ 
+ document.getElementById('searchInput').addEventListener('input', function() {
    var searchTerm = this.value.toLowerCase();
    var storedVetor = JSON.parse(localStorage.getItem('meuVetor'));
    var searchResults = [];
@@ -41,42 +41,47 @@ document.getElementById('searchInput').addEventListener('input', function() {
    var resultsElement = document.getElementById('searchResults');
    resultsElement.innerHTML = '';
  
-   for (var j = 0; j < searchResults.length; j++) {
+   if (searchResults.length === 0) {
      var li = document.createElement('li');
-     
-     var link = document.createElement('a');
-     var itemText = searchResults[j];
- 
-     if (itemURLs.hasOwnProperty(itemText)) {
-       link.href = itemURLs[itemText];
-     } else {
-       link.href = '#'; // Defina um URL padrão para itens não mapeados
-     }
-     li.classList.add('searchResult');
- 
-     link.textContent = itemText;
- 
-     link.addEventListener('click', function() {
-       window.location.href = this.href;
-     });
- 
-     li.appendChild(link);
+     li.textContent = 'Nenhuma planta encontrada';
      resultsElement.appendChild(li);
+   } else {
+     for (var j = 0; j < searchResults.length; j++) {
+       var li = document.createElement('li');
+ 
+       var link = document.createElement('a');
+       var itemText = searchResults[j];
+ 
+       if (itemURLs.hasOwnProperty(itemText)) {
+         link.href = itemURLs[itemText];
+       } else {
+         link.href = '#'; // Defina um URL padrão para itens não mapeados
+       }
+       li.classList.add('searchResult');
+ 
+       link.textContent = itemText;
+ 
+       link.addEventListener('click', function() {
+         window.location.href = this.href;
+       });
+ 
+       li.appendChild(link);
+       resultsElement.appendChild(li);
+     }
    }
-
+ 
    document.addEventListener('click', function(event) {
-      var searchBar = document.getElementById('searchInput');
-      var searchResults = document.getElementById('searchResults');
-    
-      // Verifica se o clique ocorreu fora da barra de pesquisa
-      if (event.target !== searchBar && !searchBar.contains(event.target)) {
-        // Limpa os resultados da pesquisa
-        searchResults.innerHTML = '';
-      }
-    });
-
+     var searchBar = document.getElementById('searchInput');
+     var searchResults = document.getElementById('searchResults');
+ 
+     // Verifica se o clique ocorreu fora da barra de pesquisa
+     if (event.target !== searchBar && !searchBar.contains(event.target)) {
+       // Limpa os resultados da pesquisa
+       searchResults.innerHTML = '';
+     }
+   });
  });
-
+ 
 
 
 
